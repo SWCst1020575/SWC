070f2795c7dee2607f33f144d70672ca6f6961fd
SWCst1020575 <st1020575@gmail.com>
2020-09-06 16:28:20 +0800

    Update autoclickTwitch.js

--- autoclickTwitch.js
+++ autoclickTwitch.js
@@ -1,5 +1,5 @@
 function remainTime() {
-	setTimeout("remainTime()", 60000);
+	setTimeout("remainTime()", 1000);
 	var element= document.getElementsByClassName('tw-button tw-button--success tw-interactive');
 	for(var i=0;i<element.length;i++){
 		element[i].click();
